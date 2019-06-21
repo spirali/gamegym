099e7c6414d6fae12b732bdb09e73ede8259c2be
Stanislav Bohm <spirali@kreatrix.org>
2019-06-21 12:36:44 +0200

    WIP

--- gamegym/algorithms/tournament.py
+++ gamegym/algorithms/tournament.py
@@ -5,20 +5,7 @@ from enum import Enum
 import math
 import itertools
 import random
-from collections import namedtuple
-
-
-class Player:
-
-    def __init__(self, name, strategy, rating=None):
-        self.name = name
-        self.strategy = strategy
-        self.rating = rating
-        self.rating_change = 0
-
-        self.wins = 0
-        self.losses = 0
-        self.draws = 0
+from collections import namedtuple, Counter
 
 
 class PlayerOrder:
@@ -27,8 +14,6 @@ class PlayerOrder:
     Random = 1
     BothSides = 2
 
-
-ResultRecord = namedtuple("Record", ["player1", "player2", "result"])
 
 
 class RandomPairing:
@@ -68,69 +53,152 @@ class AllPlayAllPairing:
             return (shuffle(pair) for pair in it)
 
 
-class PlayerDatabase:
+ResultRecord = namedtuple("Record", ["tournament_id", "player1", "player2", "result"])
 
-    def __init__(self,
-                 game,
-                 player_order: PlayerOrder = PlayerOrder.Fixed,
-                 max_moves=None,
-                 rng=None, seed=None):
-        assert game.players == 2
-        self.players = {}
-        self.results = []
-        self.game = game
-        self.max_moves = max_moves
-        self.player_order = player_order
-        self.rng = get_rng(rng=rng, seed=seed)
+class GameResults:
 
-    def add_player(self, name, strategy, rating=1500):
-        assert name not in self.players
-        self.players[name] = Player(name, strategy, rating)
+    def __init__(self):
+        self.records = []
 
-    def _compute_elo(self, player1, player2, elo_k, result_p1, result_p2):
-        r1 = 10 ** (player1.rating / 400)
-        r2 = 10 ** (player2.rating / 400)
+    def save(self, filename):
+        pass
+
+    @classmethod
+    def load(self, filename):
+        pass
+
+    def tournaments(self):
+        return set(r.tournament for r in self.records)
+
+    def players(self):
+        return set(r.player1 for r in self.records) | set(r.player2 for r in self.records)
+
+    def add_result(self, tournament_id, player1, player2, result):
+        self.records.append(ResultRecord(tournament_id, player1, player2, result))
+
+    def tournament_pairings(self, tournament_id):
+        for r in self.records:
+            if r.tournament_id == tournament_id:
+                yield (r.player1, r.player2)
+
+    def _compute_elo(self, player1_rating, player2_rating, elo_k, result):
+        if result == 0:
+            result_p1 = 0.5
+            result_p2 = 0.5
+        elif result > 0:
+            result_p1 = 1
+            result_p2 = 0
+        else:
+            result_p1 = 0
+            result_p2 = 1
+
+        r1 = 10 ** (player1_rating / 400)
+        r2 = 10 ** (player2_rating / 400)
         rs = r1 + r2
         e1 = r1 / rs
         e2 = r2 / rs
-        player1.rating_change += elo_k * (result_p1 - e1)
-        player2.rating_change += elo_k * (result_p2 - e2)
+        return elo_k * (result_p1 - e1), elo_k * (result_p2 - e2)
 
-    def play_tournament(self, pairing_generator, elo_k=1):
-        players = list(self.players.values())
+    def player_stats(self, initial_rating=1500, elo_k=1):
+        import pandas as pd
+        players = sorted(self.players())
+
+        frame = pd.DataFrame(index=players)
+        frame["rating"] = float(initial_rating)
+        frame["wins"] = 0
+        frame["losses"] = 0
+        frame["draws"] = 0
+
+        def get_tournamen_id(record):
+            return record.tournament_id
+
+        self.records.sort(key=get_tournamen_id)
+        frames = []
+        for tournament_id, group in itertools.groupby(self.records, key=get_tournamen_id):
+            elo_change = {}
+            for r in group:
+                p1 = frame.loc[r.player1]
+                p2 = frame.loc[r.player2]
+                elo_change1, elo_change2 = self._compute_elo(p1["rating"], p2["rating"], elo_k, r.result)
+
+                elo_change.setdefault(r.player1, 0)
+                elo_change.setdefault(r.player2, 0)
+                elo_change[r.player1] += elo_change1
+                elo_change[r.player2] += elo_change2
+
+                if r.result == 0:
+                    frame.loc[r.player1, "draws"] += 1
+                    frame.loc[r.player2, "draws"] += 1
+                elif r.result > 0:
+                    frame.loc[r.player1, "wins"] += 1
+                    frame.loc[r.player2, "losses"] += 1
+                    #p1.wins += 1
+                    #p2.losses += 1
+                else:
+                    frame.loc[r.player1, "losses"] += 1
+                    frame.loc[r.player2, "wins"] += 1
+                    #p2.wins += 1
+                    #p1.losses += 1
+
+            for p, v in elo_change.items():
+                frame.loc[p, "rating"] = max(100, frame.loc[p, "rating"] + v)
+
+            #print(frame)
+
+            new_frame = frame.copy()
+            new_frame["tournament_id"] = tournament_id
+            new_frame["player"] = new_frame.index
+            new_frame.reset_index(inplace=True, drop=True)
+            frames.append(new_frame)
+        return pd.concat(frames)
+
+
+class PlayerList:
+
+    def __init__(self,
+                 game,
+                 max_moves=None,
+                 game_results=None,
+                 rng=None,
+                 seed=None):
+        assert game.players == 2
+        self.players = {}
+        self.game = game
+        self.max_moves = max_moves
+        self.rng = get_rng(rng=rng, seed=seed)
+        self.game_results = game_results or GameResults()
+
+
+    def add_player(self, name, strategy):
+        assert isinstance(name, str)
+        assert name not in self.players
+        self.players[name] = strategy
+
+
+    def play_tournament(self, tournament_id, pairing_generator, skip_existing=False):
+        assert isinstance(tournament_id, int)
+        game_results = self.game_results
+        players = list(self.players)
         if len(players) < 2:
             raise Exception("Not enough players")
 
-        for player1, player2 in pairing_generator.generate_pairing(players):
-            self._play_match(player1, player2, elo_k)
+        pairing = pairing_generator.generate_pairing(players)
 
-        for player in players:
-            player.rating = max(100, player.rating + player.rating_change)
-            player.rating_change = 0
+        if skip_existing:
+            existing = Counter(game_results.tournament_pairings(tournament_id))
+            pairing_counter = Counter(pairing)
+            pairing = pairing_counter.subtract(existing).elements()
 
-    def get_player_table(self):
-        return [(p.name, p.rating, p.wins, p.draws, p.losses)
-                for p in self.players.values()]
+        for player1, player2 in pairing:
+            result = self._play_match(player1, player2)
+            game_results.add_result(tournament_id, player1, player2, result)
 
-    def _play_match(self, player1, player2, elo_k):
-        value = play_strategies(self.game, [player1.strategy, player2.strategy], rng=self.rng).payoff
+
+    def _play_match(self, player1, player2):
+        value = play_strategies(self.game, [self.players[player1], self.players[player2]], rng=self.rng).payoff
         if abs(value[0] - value[1]) < 0.00001:
-            player1.draws += 1
-            player2.draws += 1
-            result_p1 = 0.5
-            result_p2 = 0.5
-            result = 0
+            return 0
         elif value[0] > value[1]:
-            player1.wins += 1
-            player2.losses += 1
-            result_p1 = 1
-            result_p2 = 0
-            result = 1
+            return 1
         else:
-            player2.wins += 1
-            player1.losses += 1
-            result_p1 = 0
-            result_p2 = 1
-            result = -1
-        self.results.append(ResultRecord(player1, player2, result))
-        self._compute_elo(player1, player2, elo_k, result_p1, result_p2)
+            return -1
